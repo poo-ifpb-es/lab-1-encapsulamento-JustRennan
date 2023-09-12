9b48f66b36d6444c8ed4971939ce5d63f4a82e5c
rennan <139676615+JustRennan@users.noreply.github.com>
2023-09-12 17:43:33 -0300

    Fiz a de funcionario

---
+++ src/main/java/Funcionario.java
@@ -0,0 +1,49 @@
+public class Funcionario {
+    private String nome;
+    private String sobrenome;
+    private float salario;
+
+    // Construtor
+    public Funcionario(String nome, String sobrenome, float salario) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.salario = salario;
+    }
+
+    // Métodos de acesso para o nome
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    // Métodos de acesso para o sobrenome
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    // Métodos de acesso para o salário
+    public void setSalario(float salario) {
+        this.salario = salario;
+    }
+
+    public float getSalario() {
+        return salario;
+    }
+
+    // Método para calcular o salário anual
+    public float getSalarioAnual() {
+        return salario * 12;
+    }
+
+    // Método para conceder aumento
+    public void concederAumento(float valorAumento) {
+        salario += valorAumento;
+    }
+}
